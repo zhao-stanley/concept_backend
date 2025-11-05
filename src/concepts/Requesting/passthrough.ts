dd0e55321ffb3a9ded57d152758743752f90e879
Stanley <stanleyhzhao@gmail.com>
2025-11-05 02:49:15 -0500

    sync

--- src/concepts/Requesting/passthrough.ts
+++ src/concepts/Requesting/passthrough.ts
@@ -26,11 +26,7 @@
 
 export const inclusions: Record<string, string> = {
   // Feel free to delete these example inclusions
-  "/api/LikertSurvey/_getSurveyQuestions": "this is a public query",
-  "/api/LikertSurvey/_getSurveyResponses": "responses are public",
-  "/api/LikertSurvey/_getRespondentAnswers": "answers are visible",
-  "/api/LikertSurvey/submitResponse": "allow anyone to submit response",
-  "/api/LikertSurvey/updateResponse": "allow anyone to update their response",
+  
 };
 
 /**
@@ -45,6 +41,5 @@ export const inclusions: Record<string, string> = {
 
 export const exclusions: Array<string> = [
   // Feel free to delete these example exclusions
-  "/api/LikertSurvey/createSurvey",
-  "/api/LikertSurvey/addQuestion",
+  
 ];
